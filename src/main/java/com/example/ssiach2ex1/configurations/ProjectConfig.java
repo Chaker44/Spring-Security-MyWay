375c97757d01da5c94e56db63178e94e935cab16
chaker elj <eljchaker232@gmail.com>
2022-10-27 16:28:59 +0100

    the new conf of autehtication and authorization in spring security

--- src/main/java/com/example/ssiach2ex1/configurations/ProjectConfig.java
+++ src/main/java/com/example/ssiach2ex1/configurations/ProjectConfig.java
@@ -3,6 +3,7 @@ package com.example.ssiach2ex1.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,28 +18,22 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter  {
 
+    @Override
+    protected  void configure(AuthenticationManagerBuilder auth)throws Exception {
+        var userDetailService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("chaker").password("1234").authorities("read").build();
+        userDetailService.createUser(user);
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
-        var user = User.withUsername("chaker").password("1234")
-                .authorities("read").build();
+        auth.userDetailsService(userDetailService).passwordEncoder(NoOpPasswordEncoder.getInstance());
 
-        userDetailsService.createUser(user);
-        return userDetailsService ;
 
     }
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.httpBasic();
-        httpSecurity.authorizeRequests().anyRequest().authenticated();
+        httpSecurity.authorizeRequests().anyRequest().permitAll();
     }
 
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
         }
 
